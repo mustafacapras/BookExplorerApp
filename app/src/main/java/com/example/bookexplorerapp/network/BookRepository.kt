ff2293545cc45584c97006cc4f73dd97e79a6dee
mustafacapras <mustafaacapras@gmail.com>
2024-12-20 15:37:54 +0100

    Updated BookRepository to handle book search using Open Library API

--- app/src/main/java/com/example/bookexplorerapp/network/BookRepository.kt
+++ app/src/main/java/com/example/bookexplorerapp/network/BookRepository.kt
@@ -4,7 +4,7 @@ import com.example.bookexplorerapp.model.BookResponse
 import retrofit2.Response
 
 class BookRepository(private val apiService: ApiService) {
-    suspend fun getBooks(): Response<BookResponse> {
-        return apiService.getBooks()
+    suspend fun searchBooks(query: String): Response<BookResponse> {
+        return apiService.searchBooks(query)
     }
 }
