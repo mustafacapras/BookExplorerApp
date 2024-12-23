457d7727432c0015eaf907fb6d77511c87b9e336
mustafacapras <mustafaacapras@gmail.com>
2024-12-23 19:24:03 +0100

    Add layout for MainActivity, Add MainActivity implementation, add favorite button

---
+++ app/src/main/java/com/example/bookexplorerapp/MainPageViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.bookexplorerapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.bookexplorerapp.network.ApiService
+
+class MainPageViewModelFactory(private val apiService: ApiService) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainPageViewModel::class.java)) {
+            return MainPageViewModel(apiService) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
